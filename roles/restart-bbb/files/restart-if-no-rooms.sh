ed1e76606533ddeedf8a9e55c303b565ea432fb4
Andreas B. Mundt <andi@debian.org>
2020-04-23 09:50:22 +0200

    Draft implementation taking BBB uptime into account.

--- roles/restart-bbb/files/restart-if-no-rooms.sh
+++ roles/restart-bbb/files/restart-if-no-rooms.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
+#
+# All instances with an uptime larger than AGE (in seconds)
+# will be restartet if there are no meetings running.
+#
+set -eu
 
-/usr/local/bin/checkbbb_for_chost.py
+AGE="${1:-0}"
 
-sleep 1
+STAMP="/var/log/bbb-restart-stamp"
+BBBUPTIME=-1
+[ -f "$STAMP" ] && BBBUPTIME="$(( $(date +%s) - $(date +%s -d $(cat $STAMP)) ))"
 
-numrooms=$(grep -o "numMeetings=[0-9]\+" /var/cache/checkbbb/overview | sed -e "s/numMeetings=//")
+if [ $BBBUPTIME -gt $AGE -o $BBBUPTIME -lt 0 ] ; then
+    /usr/local/bin/checkbbb_for_chost.py
+    sleep 1
 
-if [ x$numrooms  == "x0" ]; then
+    numrooms=$(grep -o "numMeetings=[0-9]\+" /var/cache/checkbbb/overview | sed -e "s/numMeetings=//")
+
+    if [ x$numrooms  == "x0" ]; then
         # restart
-        bbb-conf --restart
+        bbb-conf --restart && echo "$(date --iso-8601='seconds')" > "$STAMP"
         echo "There were $numrooms rooms. BBB restarted"
-else
+    else
         echo "There were $numrooms rooms. BBB NOT restarted"
+    fi
+else
+    echo "BBB uptime too small to restart."
 fi
-
