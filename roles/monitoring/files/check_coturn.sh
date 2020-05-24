325a960c16ce16259dc10fb09255ba5cf9f7e772
Frank Schiebel <frank@linuxmuster.net>
2020-05-24 12:39:41 +0200

    Added check for alerting non answering coturns

---
+++ roles/monitoring/files/check_coturn.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+tmpfile=$(mktemp)
+turnutils_uclient -t -n 1000 -m 10 -l 3037 -e 127.0.0.1 localhost > $tmpfile 2>&1 &
+
+sleep 1
+
+kill $!
+wait $! 2>/dev/null
+
+OKLINE=$(grep ERROR: $tmpfile | grep Allocation)
+rm $tmpfile
+
+if [ "x$OKLINE" != "x" ]; then 
+	echo "0  Coturn  -  Coturn answered."
+else
+	echo "2  Coturn  -  No answer from coturn."
+fi
+
+
